c1c62c0690ea24071c73b35a6803fcd9bf229e8c
fyousry <fadyyousry2018@gmail.com>
2020-08-12 23:31:30 +0200

    remove static.json

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
 import ActionCable from 'action-cable-react-jwt';
 import auth from './auth';
 
-const url = 'https://free4chat-api.herokuapp.com';
+const url = 'http://localhost:4000';
 
 let Consumer = {};
 Consumer.cable = ActionCable.createConsumer(url + "/cable", auth.jwt());
